f19cbf26b4ce6a07c19af56c3ac5f7e548751287
hippydream <hippydream@a47034e0-0190-11de-9d1a-b59b2e1864b6>
2007-09-19 11:12:25 +0000

    Added blue.mpi plugin. WARNING: work-in-progress version!

---
+++ src/mpiblue/blueglobal.h
@@ -0,0 +1,40 @@
+/**************************************************************************
+** Filename: blueglobal.h
+** Revision: 3
+**
+** Copyright (C) 2007 Angius Fabrizio. All rights reserved.
+**
+** This file is part of the Movida project (http://movida.sourceforge.net/).
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See the file LICENSE.GPL that came with this software distribution or
+** visit http://www.gnu.org/copyleft/gpl.html for GPL licensing information.
+**
+**************************************************************************/
+
+#ifndef MPI_BLUEGLOBAL_H
+#define MPI_BLUEGLOBAL_H
+
+#include "global.h"
+#include <QtGlobal>
+
+#ifndef MPI_EXPORT_BLUE
+# ifdef Q_OS_WIN
+#  if defined(MPI_BUILD_BLUE_DLL)
+#   define MPI_EXPORT_BLUE __declspec(dllexport)
+#  else
+#   define MPI_EXPORT_BLUE __declspec(dllimport)
+#  endif // MPI_BUILD_BLUE_DLL
+# else // Q_OS_WIN
+#  define MPI_EXPORT_BLUE
+# endif
+#endif // MPI_EXPORT_BLUE
+
+#endif // MPI_BLUEGLOBAL_H
